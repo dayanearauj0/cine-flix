11b18c84a64642e222f7561f9c04707368f2b8bd
dayane <dayanecarla72@gmail.com>
2023-12-25 23:41:16 -0300

    :microscope: Experimento

---
+++ src/catalogo.js
@@ -0,0 +1,11 @@
+const filmes = ['Mulan','Frozen','Shrek','Matrix','Gladiador','Interstellar','Jurassic Park'];
+function exibirCatalogo(filmes){
+  console.log('Mulan');//console.log(str.slice(4, 19))
+  console.log('Frozen');
+  console.log('Shrek');
+  console.log('Matrix');
+  console.log('Gladiador');
+  console.log('Interstellar');
+  console.log('Jurassic Park');
+}
+exibirCatalogo();
